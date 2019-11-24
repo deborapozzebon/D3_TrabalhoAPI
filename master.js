655a21ea0b5ec7887cd5c7000bd27fc554ce35a5
Débora Pozzebon <debora.pozzebon@outlook.com>
2019-11-24 19:03:53 -0300

    hsauhsufd

--- master.js
+++ master.js
@@ -16,7 +16,25 @@ function fetchWithParameters(coin, ticker) {
     });
 }
 
-function calulateBitcoin() {
+function calulateBitcoin2() {
+  fetch('https://www.mercadobitcoin.net/api/' + coin + '/trades/', { method: 'GET' })
+    .then(function (response) {
+      response.json().then(function (data) {
+        returnValueCalulateBitcoin(data);
+      });
+    })
+    .catch(function (err) {
+      console.error('Failed retrieving information', err);
+    });
+}
+
+function calulateBitcoin(coin) {
+  if (isEmpty(coin)) {
+    coin = 'BTC';
+  }
+
+  console.log(coin);
+
   fetch('https://www.mercadobitcoin.net/api/' + coin + '/trades/', { method: 'GET' })
     .then(function (response) {
       response.json().then(function (data) {
@@ -31,6 +49,10 @@ function calulateBitcoin() {
 //#endregion
 
 //#region functions with data parameter
+function isEmpty(str) {
+  return (!str || 0 === str.length);
+}
+
 function getHighAndLowerValue(data) {
   var $myDivHigh = $("#high");
   var $myDivLow = $("#low");
@@ -44,18 +66,17 @@ function getHighAndLowerValue(data) {
 
 function returnValueCalulateBitcoin(data) {
   var $myDivValueInReal = $("#valueInReal");
-  console.log(data[0].price);
   var h6_valueInReal = "<h6>Quatidade que pode ser comprada: " + document.getElementById('valueRS').value / data[0].price + "</h6>";
 
   $myDivValueInReal.html(h6_valueInReal);
 }
 
+function myFunction() {
+  fetchWithParameters(document.getElementById("l").value, 'ticker');
+  calulateBitcoin(document.getElementById("l").value);
+}
 
 function calulateNewValueFromCurrentCoin(data) {
-  console.log(data);
+  console.log(data.value);
 }
-  //console.log(aaaa);
-  //console.log($('#exampleList').val())
-//}
-
 //#endregion
